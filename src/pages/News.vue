4bd9aff9eafb9eba658056a5237599e5d45be5d3
AleksanderSkubala <alexskubala@gmail.com>
2020-09-05 22:34:26 +0200

    add news page

---
+++ src/pages/News.vue
@@ -0,0 +1,48 @@
+<template>
+  <Layout>
+    <div class="posts">
+      <PostCard v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node"/>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allPost(filter: { published: { eq: true }}) {
+    edges {
+      node {
+        id
+        title
+        date (format: "D. MMMM YYYY")
+        timeToRead
+        description
+        cover_image (width: 770, height: 380, blur: 10)
+        ...on Post {
+        id
+        title
+        path
+        }
+        path
+        tags {
+          id
+          title
+          path
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import PostCard from '~/components/PostCard.vue'
+
+export default {
+  components: {
+    PostCard
+  },
+  metaInfo: {
+    title: 'Aktualności'
+  }
+}
+</script>
